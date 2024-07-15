221272536a9b7ddb38cb0895a88ab3eda1fb8acc
jerias2018 <163367532+jerias2018@users.noreply.github.com>
2024-07-15 15:57:47 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+// 変数に値を代入する
+  $score1= 80;
+  $score2= 60;
+  $score3= 55;
+  $score4= 40;
+  $score5= 100;
+  $score6= 25;
+  $score7= 80;
+  $score8= 95;
+  $score9= 30;
+  $score10=60;
+// 合計点を計算する
+  $total_testscore= $score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10;
+// 平均点を計算する
+  $average_testscore= $total_testscore / 10;
+
+// 変数の中身を出力する
+  echo $average_testscore;
+    ?>
+  </p>
+</body>
+
+</html>
